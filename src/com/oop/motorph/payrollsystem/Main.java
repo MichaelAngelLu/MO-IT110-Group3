ac3679f8cef9ce485a1102bf7bbf57e3ea2f707e
zephyrstorm123 <quitay.anthony@gmail.com>
2024-03-07 23:17:22 +0800

    Added Leave Credits Page

---
+++ src/com/oop/motorph/payrollsystem/Main.java
@@ -0,0 +1,19 @@
+package com.oop.motorph.payrollsystem;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+//Ignore this block. For debug and testing purposes
+//		PayrollHomepage payroll = new PayrollHomepage();
+//		payroll.displayHomepage();
+//		Employee employeeDetails = new Employee();
+//		employeeDetails.displayEmployeesAdmin();
+//		System.out.println(employeeDetails.employee[0].getFirstName());
+		
+		//Prompt user to login after application startup
+		UserLogin login = new UserLogin();
+		login.greetUser();
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,10 @@
+/**
+ * 
+ */
+/**
+ * @author Michael
+ *
+ */
+module OOPMotorPHPayrollSystem {
+	requires opencsv;
+}
